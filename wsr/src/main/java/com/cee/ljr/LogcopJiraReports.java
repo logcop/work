45d5d545204185daf644d87d3c3a1bf5eba9a558
chuck <c.emmons@gmail.com>
2016-09-20 14:19:46 -1000

    update from home

--- wsr/src/main/java/com/cee/ljr/LogcopJiraReports.java
+++ wsr/src/main/java/com/cee/ljr/LogcopJiraReports.java
@@ -11,11 +11,13 @@ public class LogcopJiraReports {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		
 		// weekly status report generation
 		String weekEndDate = "9/11/2016";
 		String wsrReportPath = null;
 		WeeklyStatusReportGenerator srGenerator = ctx.getBean(WeeklyStatusReportGenerator.class);
 		srGenerator.generateV2(weekEndDate, wsrReportPath);
+		
 		//String sprintNumber = "13";
 		//srGenerator.generateV1(weekEndDate);
 		
